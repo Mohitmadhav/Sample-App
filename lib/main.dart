859fbfc64dfe6dd462d83e93c375473c401f286d
Mohit Madhav <mohitmadhav88@gmail.com>
2021-01-03 23:48:23 +0530

    Chapter 1 done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Sample App", //This is shown when the app is minimized
     home: HomePage(),
   ));
@@ -11,12 +12,17 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Sample App"),
-      ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Sample App"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Hi Flutter"),
+          ),
+        ),
       ),
     );
   }
